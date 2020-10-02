f801ba1a5edef6dc1d2f50d942e963d7db2d8f8c
Anton Tymoshchuk <aatymoshchuk@gmail.com>
2020-10-02 14:00:54 +0300

    Add project files.

---
+++ VolumeController/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using AudioSwitcher.AudioApi.CoreAudio;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace VolumeController
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static CoreAudioDevice CoreAudioDevice { get; set; }
+    }
+}
---
+++ VolumeController/VolumeControl.xaml.cs
@@ -0,0 +1,53 @@
+﻿using AudioSwitcher.AudioApi.CoreAudio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VolumeController
+{
+    /// <summary>
+    /// Логика взаимодействия для VolumeControl.xaml
+    /// </summary>
+    public partial class VolumeControl : UserControl
+    {
+        public static readonly DependencyProperty DefaultVolumeProperty = DependencyProperty.RegisterAttached("DefaultVolume", typeof(int), typeof(VolumeControl), new FrameworkPropertyMetadata(50, OnVolumeChanged));
+        public int DefaultVolume
+        {
+            get { return Convert.ToInt32(GetValue(DefaultVolumeProperty)); }
+            set { SetValue(DefaultVolumeProperty, value); }
+        }
+
+        public VolumeControl()
+        {
+            InitializeComponent();
+            CoreAudioController coreAudioConteroller = new CoreAudioController();
+            App.CoreAudioDevice = coreAudioConteroller.GetDefaultDevice(AudioSwitcher.AudioApi.DeviceType.Playback, AudioSwitcher.AudioApi.Role.Multimedia);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            popup.IsOpen = true;
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            App.CoreAudioDevice.Volume = e.NewValue;
+        }
+
+        private static void OnVolumeChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            App.CoreAudioDevice.Volume = Convert.ToInt32(e.NewValue);
+        }
+    }
+}
